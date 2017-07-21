b683b9eac787f38b208d47aa6335c0ee66979d47
Glitchgamer12 <30339375+Glitchgamer12@users.noreply.github.com>
2017-07-21 17:33:40 +0800

    added files

---
+++ factions.lua
@@ -0,0 +1,2 @@
+{20={color0=0x101010, color1=0x7c7c7c, primaries=2, aiflags=METAMORPHOSIS|RECKLESS|WANDER|DODGES|TRACTOR_TRANSIENT,
+    playable=2, name="IRONBASS", thrustSFX=1}}
---
+++ regions.lua
@@ -0,0 +1,2 @@
+{ident=20, color=0x5e6d91, faction=20, count=15, position={0.1, 1}, radius={0.1, 0.15},
+  fleets={{8, {{0, 3500}, {1, 1000}}}}, fleetCount={{4, 8}}, ambient={0}}
